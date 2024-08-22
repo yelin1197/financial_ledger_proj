b2d5e2a143e3a0418e2cd65c4228d71a12881b47
hwup <hvvijung@gmail.com>
2024-08-23 00:41:44 +0900

    init

---
+++ data/mock.js
@@ -0,0 +1,43 @@
+const data = [
+    {
+      title: '여행 가기',
+      expense: '10000000',
+      isComplete: false,
+      createdAt: new Date('2023-03-23T06:34:07.617Z'),
+      updatedAt: new Date('2023-03-23T06:34:07.617Z'),
+    },
+    {
+      title: 'code it 결제',
+      expense: '43000',
+      description: 'Tic Tac Toe 게임 완성하기',
+      isComplete: true,
+      createdAt: new Date('2023-03-23T06:34:08.617Z'),
+      updatedAt: new Date('2023-03-23T06:34:08.617Z'),
+    },
+    {
+      title: '책 구매',
+      expense: '23000',
+      isComplete: true,
+      createdAt: new Date('2023-03-23T06:34:09.617Z'),
+      updatedAt: new Date('2023-03-23T06:34:09.617Z'),
+    },
+    {
+      title: '밥',
+      expense: '30000',
+      description: '30 페이지',
+      isComplete: false,
+      createdAt: new Date('2023-03-23T06:34:10.617Z'),
+      updatedAt: new Date('2023-03-23T06:34:10.617Z'),
+    },
+    {
+      title: '헤드셋',
+      expense: '56000',
+      description: '사고싶었던 소니 헤드셋',
+      isComplete: false,
+      createdAt: new Date('2023-03-23T06:34:11.617Z'),
+      updatedAt: new Date('2023-03-23T06:34:11.617Z'),
+    },
+  ];
+  
+  export default data;
+  
